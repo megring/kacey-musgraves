f80d56e0fd957f6825dbe7a7cdf83979e37c17ed
Megan Ring <meg.ring@gmail.com>
2020-03-09 13:33:21 -0700

    Updates

---
+++ precache-manifest.4a418e0ea16541efbdc37228056f4c2d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d1d5d510992fa268be03306f691055",
+    "url": "/kacey-musgraves/index.html"
+  },
+  {
+    "revision": "b2afe09976071209d5bd",
+    "url": "/kacey-musgraves/static/css/main.dd9d3e9b.chunk.css"
+  },
+  {
+    "revision": "9b55820cf2fc4beed1b9",
+    "url": "/kacey-musgraves/static/js/2.1cae2a12.chunk.js"
+  },
+  {
+    "revision": "b2afe09976071209d5bd",
+    "url": "/kacey-musgraves/static/js/main.f236c0b6.chunk.js"
+  },
+  {
+    "revision": "743737bde02260c04092",
+    "url": "/kacey-musgraves/static/js/runtime-main.284c0082.js"
+  },
+  {
+    "revision": "e22fa01cc321d6bfe0a09d031ee39167",
+    "url": "/kacey-musgraves/static/media/GoldenHour.e22fa01c.jpg"
+  },
+  {
+    "revision": "632805c863ec80a743bfdb3b58a76140",
+    "url": "/kacey-musgraves/static/media/KaceyXMas.632805c8.jpg"
+  },
+  {
+    "revision": "7f1bcfc7c1cb993dd6b2c278f7915ee9",
+    "url": "/kacey-musgraves/static/media/PageantMaterial.7f1bcfc7.jpg"
+  },
+  {
+    "revision": "9a6dbf16bfd1ff5b5dbd59d3724e320d",
+    "url": "/kacey-musgraves/static/media/SameTrailer.9a6dbf16.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/kacey-musgraves/static/media/logo.25bf045c.svg"
+  }
+]);
